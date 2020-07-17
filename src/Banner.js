58de8219c9b7418ec63a021cae30c9bc59ff8c6c
amooryjubran <omarsalehjubran@gmail.com>
2020-07-17 15:58:42 -0400

    displaying images style + Banner setup

---
+++ src/Banner.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+export default function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {}
+    fetchData();
+  }, []);
+
+  return (
+    <header>
+      {/* title */}
+      {/* title */}
+      {/* title */}
+    </header>
+  );
+}
--- src/Row.js
+++ src/Row.js
@@ -3,7 +3,7 @@ import axios from "./axios";
 import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
-export default function Row({ title, fetchURL }) {
+export default function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   // A snippet of code which runs based on a specific confition/variable
@@ -27,8 +27,10 @@ export default function Row({ title, fetchURL }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
