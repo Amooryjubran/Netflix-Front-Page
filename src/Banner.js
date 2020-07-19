6f5502d40ff48de3b888422e547fdf556e13edd3
amooryjubran <omarsalehjubran@gmail.com>
2020-07-19 13:30:53 -0400

    banner set up + style + extra

--- src/Banner.js
+++ src/Banner.js
@@ -20,6 +20,10 @@ export default function Banner() {
     fetchData();
   }, []);
 
+  function truncateWithEllipses(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   console.log(movie);
   return (
     <header
@@ -31,12 +35,16 @@ export default function Banner() {
       }}
     >
       <div className="banner__contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner__description">{movie?.overview}</h1>
+        <h1 className="banner_description">
+          {truncateWithEllipses(movie?.overview, 150)}
+        </h1>
       </div>
     </header>
   );
