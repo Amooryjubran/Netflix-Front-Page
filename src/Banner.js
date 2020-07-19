9c4b4c84f5a25bd2de4054958a5696243be37c93
amooryjubran <omarsalehjubran@gmail.com>
2020-07-19 12:55:18 -0400

    banner set up

--- src/Banner.js
+++ src/Banner.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
 
 export default function Banner() {
-  const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState([]); // to get a random movie everytime you refresh the page
 
   useEffect(() => {
-    async function fetchData() {}
-    fetchData();
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      console.log(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
   }, []);
 
   return (
