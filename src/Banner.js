9987aba12912f2635cf2ff25388bae6f13e2d28e
amooryjubran <omarsalehjubran@gmail.com>
2020-07-19 13:16:36 -0400

    banner set up + style

--- src/Banner.js
+++ src/Banner.js
@@ -1,26 +1,43 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 export default function Banner() {
-  const [movie, setMovie] = useState([]); // to get a random movie everytime you refresh the page
+  // to get a random movie everytime you refresh the page
 
+  const [movie, setMovie] = useState([]);
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(requests.fetchNetflixOriginals);
-      console.log(
+      setMovie(
         request.data.results[
           Math.floor(Math.random() * request.data.results.length - 1)
         ]
       );
+      return request;
     }
+    fetchData();
   }, []);
 
+  console.log(movie);
   return (
-    <header>
-      {/* title */}
-      {/* title */}
-      {/* title */}
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <div className="banner__buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner__description">{movie?.overview}</h1>
+      </div>
     </header>
   );
 }
